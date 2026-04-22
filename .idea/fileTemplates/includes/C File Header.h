3b40185f77a35d5830b6c577a5a70bf94f48cb55
leojay <crazyleojay@163.com>
2026-04-22 20:05:34 +0800

    update

---
+++ .idea/fileTemplates/includes/C File Header.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright [2026] @github-crazyleojay (crazyleojay@163.com/gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#if ($HEADER_COMMENTS)
+/**
+ * Created by $USER_NAME on ${DATE}.
+#if ($ORGANIZATION_NAME && $ORGANIZATION_NAME != "")
+ * Copyright (c) $YEAR ${ORGANIZATION_NAME}#if (!$ORGANIZATION_NAME.endsWith(".")).#end All rights reserved.
+#end
+ *
+ * @author leojay`fu
+ * @email crazyleojay@163.com
+ * @url https://github.com/CrazyLeoJay
+ */
+#end
+
