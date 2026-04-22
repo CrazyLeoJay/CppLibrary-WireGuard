cd6153f9197594305274e34c1945518d89f47456
leojay <crazyleojay@163.com>
2026-04-22 14:53:08 +0800

    将ui和通道实现分开

---
+++ platform-harmonyos/.idea/fileTemplates/internal/C Header File.h
@@ -0,0 +1,6 @@
+#parse("myDest.txt")
+#parse("C File Header.h")
+#[[#ifndef]]# ${UPPER_PROJECT_NAME}_${UPPER_NAME}_H
+#[[#define]]# ${UPPER_PROJECT_NAME}_${UPPER_NAME}_H
+
+#[[#endif]]# //${UPPER_PROJECT_NAME}_${UPPER_NAME}_H
