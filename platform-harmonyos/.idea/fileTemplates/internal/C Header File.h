7600732a8b02683aeabff456441936e9c019c782
leojay <crazyleojay@163.com>
2026-05-25 15:32:29 +0800

    在c 分支上移除其他平台代码

--- platform-harmonyos/.idea/fileTemplates/internal/C Header File.h
+++
@@ -1,6 +0,0 @@
-#parse("myDest.txt")
-#parse("C File Header.h")
-#[[#ifndef]]# ${UPPER_PROJECT_NAME}_${UPPER_NAME}_H
-#[[#define]]# ${UPPER_PROJECT_NAME}_${UPPER_NAME}_H
-
-#[[#endif]]# //${UPPER_PROJECT_NAME}_${UPPER_NAME}_H
